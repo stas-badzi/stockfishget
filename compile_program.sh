2e23a6f537a3d76a80c450e9d0a474fca59bc58e
stas-badzi <151390437+stas-badzi@users.noreply.github.com>
2024-03-16 18:25:00 +0000

    v1.0.4

--- compile_program.sh
+++ compile_program.sh
@@ -5,12 +5,11 @@ cd ./src/
 if [ "$OSTYPE" == "linux-gnu" ]
 then
     chmod -x ./scripts/build-stockfish.sh
-    chmod -x ./scripts/program.sh
     chmod -x ./scripts/run.sh
     chmod -x ./scripts/compile.sh
 fi
 
-#bash ./scripts/build-stockfish.sh
+bash ./scripts/build-stockfish.sh
 
 bash ./scripts/compile.sh > ../null.log
 rm -f ../null.log
